9a621268bca68debdf65b088c4b821830a1c6b8a
SphereBorder <2460274307@qq.com>
2020-02-14 21:52:14 +0800

    断言与静态断言

--- 源.cpp
+++ 源.cpp
@@ -1,12 +1,48 @@
 #include <iostream>
-#include "test.h"
-#include "text.h"
+#include <vector>
+#include <Initializer_list>
+//#define NDEBUG 
+#include <assert.h>
+//#include "test.h"
+//#include "text.h"
 using namespace std;
+
+class vec
+{
+public:
+	vector<int> obj;
+	vec() = default;
+	vec(std::initializer_list<int> arry)
+	{
+		cout << "初始化列表构造了" << endl;
+		for (auto i = arry.begin(); i != arry.end(); i++)
+		{
+			obj.push_back(*i);
+		}
+	}
+	vec(const vec& vc) :obj(vc.obj)
+	{
+		cout << "拷贝构造了" << endl;
+	}
+	int& operator[](int index)
+	{
+		assert(index >= 0 && index < obj.size());
+		return obj[index];
+	}
+	~vec()
+	{
+		cout << "析构了" << endl;
+	}
+}; 
+
 
 void main()
 {
-	test ts;
-	text tx;
-	cout << ts.age << endl;
-	cout << tx.bird << endl;
+	vec v{ 1, 2, 3, 4, 5, 6 };
+	cout << v[3] << endl;
+	v[2] = 10;
+	vec v2(v);
+	cout << v2[2] << endl;
+	const int i = 3;
+	static_assert(i != 2, "出错");
 }
